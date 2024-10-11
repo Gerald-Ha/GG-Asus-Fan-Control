3cac1c2c22890ba717905fef2b90c2d5e04e8b4e
Gerald-H <53166232+Gerald-Ha@users.noreply.github.com>
2024-10-11 14:51:20 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-
 INSTALL_DIR="/opt/GG-Asus-Fan-Control"
-SERVICE_FILE="gg-asus-fan-control.service"  
-SYMLINK="/usr/local/bin/GG-Fan"
-
+SERVICE_FILE="gg-asus-fan-control.service"
+SYMLINK="/usr/local/bin/gg-fan"
 
 sudo systemctl stop "$SERVICE_FILE"
 sudo systemctl disable "$SERVICE_FILE"
 
-
 sudo rm /etc/systemd/system/"$SERVICE_FILE"
 sudo systemctl daemon-reload
 
-
 sudo rm -f "$SYMLINK"
-
 
 sudo rm -rf "$INSTALL_DIR"
 
